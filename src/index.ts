911d8507e1ee4132fe98710e4a8b431d31a93541
unknown <janehopeodonnell@gmail.com>
2022-07-11 10:42:42 -0700

    install day.js and add helper file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
+// What is the starting date and time (in UTC) of the earliest interval where any of the workers are free?
 export async function solveFirstQuestion(
   inputFilePath: string
 ): Promise<string> {
   // TODO: Solve me!
+
+
   return "";
 }
 
+// What is the ending date and time (in UTC) of the latest interval where any of the workers are free?
 export async function solveSecondQuestion(
   inputFilePath: string
 ): Promise<string> {
   // TODO: Solve me!
+
+
   return "";
 }
 
+//What are the intervals of date and times (in UTC) where there are at least 2 workers free? Order your results in ascending order starting from the interval with the earliest start date and time and ensure your results are continuous, merging any overlapping intervals into a single interval where required.
 export async function solveThirdQuestion(
   inputFilePath: string
 ): Promise<string[]> {
   // TODO: Solve me!
+
+  
   return [];
 }
